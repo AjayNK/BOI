a7f663aa13fb8cca4d34147c04a266299ce2feb6
AjayNK <ajay.thirumalesh@gmail.com>
2021-06-23 17:47:04 +0530

    Commit Version 3.0

--- BankOfIndia/src/test/java/code/TestScript.java
+++ BankOfIndia/src/test/java/code/TestScript.java
@@ -1,12 +1,19 @@
 package code;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestScript {
 
 	 @Test
-	 public static void print() {
-		 System.out.println("hello");
+	 public static void print() throws InterruptedException {
+			System.setProperty("webdriver.chrome.driver", "C:\\selenium_jars\\browser_drivers\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.facebook.com");		
+			driver.manage().window().maximize();
+			Thread.sleep(3000);
+			driver.close();
 	 }
 
 }
